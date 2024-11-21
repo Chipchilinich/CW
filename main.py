ff56f7a666568b5b699b808da025487c564c9d54
Your Name <zingorskiy@gmail.ru>
2024-11-21 20:13:06 +0300

    corse fix2

--- main.py
+++ main.py
@@ -1,67 +1,110 @@
-from src.get_vacancy import HH
-from src.config import config
-from src.DB_class import DBManager
-from src.ulils import create_database, save_data_to_database
+import json
+import os
+from datetime import datetime
+
+from dotenv import load_dotenv
+
+from src.clase_2 import HeadHunterAPI
+from src.clase_1 import DBManager
+
+load_dotenv()
 
 
-def main():
-    """Функция для работы прогрммы"""
-    params = config()
+# def main():
+#
+#     # Установка соединения
+#
+#     dbname='your_database',
+#     user='postgres',
+#     password='123qweewq321',
+#     host='localhost',
+#     port='5432'
+#
+#     bd = DBManager(bd_name=dbname, user=user, password=password)
+#     bd.create_tables()
+#     # bd.close()
 
-    data_employer = HH().get_employers()
-    data_vacancies = HH().load_vacancies()
-    create_database('hh_db', params)
-    save_data_to_database(data_employer, data_vacancies, 'hh_db', params)
-    db_manager = DBManager(params)
 
-    print("""
-        Введите цифру для получения нужной Вам информации
-        0 - Завершить программу
-        1 - получает список всех компаний и количество вакансий у каждой компаний.
-        2 - получает список всех вакансий с указанием названия компании,
-        названия вакансии и зарплаты и ссылки на вакансию.
-        3 - получает среднюю зарплату по вакансиям.
-        4 - получает список всех вакансий, у которых зарплата выше средней по всем вакансиям.
-        5 - получает список всех вакансий, в названии которых содержатся переданные в метод слова, например python.
-    """)
+def user_interaction():
+
+    hh_api = HeadHunterAPI()
+    # Подключение к базе данных
+    db = DBManager(os.getenv("DB_NAME"), os.getenv("DB_USER"), os.getenv("DB_PASSWORD"))
+
+    # Проверяем наличие таблиц
+    # if not db.tables_exist():
+    db.create_tables()
+    #     print("Таблицы созданы.")
+
+    # Загружаем компании из JSON
+    db.load_companies_from_json("data/companies.json")
+
+    with open("data/companies.json") as f:
+        copas = json.load(f)
+        for i in copas:
+            db.load_vac_from_json(hh_api.get_company_vacancies(i.get("employer_id")))
+
     while True:
-        user_input = input()
-        if user_input == "1":
-            companies_and_vacancies_count = db_manager.get_companies_and_vacancies_count()
-            print("Cписок всех компаний и количество вакансий у каждой компаний:")
-            for i in companies_and_vacancies_count:
-                print(i)
-            print("Введите цифру для получения нужной Вам информации")
-        elif user_input == "2":
-            all_vacancies = db_manager.get_all_vacancies()
-            print("""
-            список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию:
-            """)
-            for i in all_vacancies:
-                print(i)
-            print("Введите цифру для получения нужной Вам информации")
-        elif user_input == '3':
-            avg_salary = db_manager.get_avg_salary()
-            print("средняя зарплату по вакансиям:")
-            print(avg_salary)
-            print("Введите цифру для получения нужной Вам информации")
-        elif user_input == "4":
-            vacancies_with_higher_salary = db_manager.get_vacancies_with_higher_salary()
-            print("список всех вакансий, у которых зарплата выше средней по всем вакансиям:")
-            for i in vacancies_with_higher_salary:
-                print(i)
-            print("Введите цифру для получения нужной Вам информации")
-        elif user_input == "5":
-            user_word = input("Введите ключ слово\n").lower()
-            vacancies_with_keyword = db_manager.get_vacancies_with_keyword(user_word)
-            print("список всех вакансий, в названии которых содержатся переданные в метод слова:")
-            for i in vacancies_with_keyword:
-                print(i)
-            print("Введите цифру для получения нужной Вам информации")
-        elif user_input == "0":
-            print("Программа завершила работу")
+        print("\nДобро пожаловать в систему управления вакансиями!")
+        print("Выберите действие:")
+        print("1. Показать список компаний и количество вакансий у каждой")
+        print("2. Показать все вакансии")
+        print("3. Показать среднюю зарплату по вакансиям")
+        print("4. Показать вакансии с зарплатой выше средней")
+        print("5. Поиск вакансий по ключевому слову")
+        print("6. Выйти")
+        print("7. Вернутся")
+
+        choice = input("Введите номер действия: ")
+
+        if choice == "1":
+            # Показать список компаний и количество вакансий
+            companies_and_vacancies = db.get_companies_and_vacancies_count()
+            for row in companies_and_vacancies:
+                print(f"Компания: {row['name']}, Количество вакансий: {row['vacancies_count']}")
+
+        elif choice == "2":
+            # Показать все вакансии
+            vacancies = db.get_all_vacancies()
+            for vac in vacancies:
+                print(
+                    f"Компания: {vac['company_name']}, Вакансия: {vac['vacancy_title']}, "
+                    f"Зарплата: от {vac['salary_min']} до {vac['salary_max']}, Ссылка: {vac['url']}"
+                )
+
+        elif choice == "3":
+            # Показать среднюю зарплату
+            avg_prise = db.get_avg_salary()
+            print(f"Средняя зарплата по всем вакансиям: {avg_prise}")
+
+        elif choice == "4":
+            # Показать вакансии с зарплатой выше средней
+            above_average = db.get_vacancies_with_higher_salary()
+            for a_avg in above_average:
+                print(
+                    f"Компания: {a_avg['company_name']}, Вакансия: {a_avg['vacancy_title']}, "
+                    f"Зарплата: от {a_avg['salary_min']} до {a_avg['salary_max']}, Ссылка: {a_avg['url']}"
+                )
+
+        elif choice == "5":
+            # Поиск вкансий р=по ключевому слову
+            keyword = input("Введите ключевое слова для поиска: ")
+            search = db.get_vacancies_with_keyword()
+            for sear in search:
+                print(
+                    f"Компания: {sear['company_name']}, Вакансия: {sear['vacancy_title']}, "
+                    f"Зарплата: от {sear['salary_min']} до {sear['salary_max']}, Ссылка: {sear['url']}"
+                )
+
+        elif choice == "6":
+            # Выйти из программы
+            db.close()
+            print("Программа завершена.")
             break
 
+        else:
+            print("Неверный выбор. Пожалуйста, попробуйте снова.")
 
-if __name__ == '__main__':
-    main()
+
+if __name__ == "__main__":
+    user_interaction()
